a0fa725632bb659b6751f061abcf3661fc8e594c
Dimitra Deliopoulos <DD907433@wcupa.edu>
2021-06-01 19:23:03 -0400

    add git stuff

--- homework.md
+++ homework.md
@@ -1,3 +1,15 @@
+# 2021-06-01
+1. 
+
+# 2021-05-31
+1. add feature for removing plants
+   a. design
+   b. HTML
+   c. API method
+   d. JavaScript
+   
+
+# Olden
 1. refactor the programming task with Set
 2. define database structure for your application
 3. create API for Leaves
--- src/main/java/ddeliopoulos/github/gardenjournal/plant/api/CreatePlantRequest.java
+++ src/main/java/ddeliopoulos/github/gardenjournal/plant/api/CreatePlantRequest.java
@@ -3,10 +3,13 @@ package ddeliopoulos.github.gardenjournal.plant.api;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @RequiredArgsConstructor
 public final class CreatePlantRequest {
 
+    @NotBlank(message = "Name is mandatory")
     private final String name;
     private final int colorRed;
     private final int colorGreen;
