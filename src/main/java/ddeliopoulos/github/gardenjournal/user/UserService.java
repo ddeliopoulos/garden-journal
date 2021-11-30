22e1060052187f97eca097b292ddc98742d3885f
ddeliopoulos <ddeliopoulos@gmail.com>
2021-11-30 12:08:18 -0500

    fixed backend google client id

--- src/main/java/ddeliopoulos/github/gardenjournal/user/UserService.java
+++ src/main/java/ddeliopoulos/github/gardenjournal/user/UserService.java
@@ -7,30 +7,37 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.Optional;
 
-@RequiredArgsConstructor
 @Service
 public class UserService {
 
-    private static final String CLIENT_ID = "826903811377-7vc3unief3g7bbn341cr06kbfvbb49no.apps.googleusercontent.com";
     private static final HttpTransport TRANSPORT = new NetHttpTransport();
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 
-    private final static GoogleIdTokenVerifier VERIFIER = new GoogleIdTokenVerifier.Builder(TRANSPORT, JSON_FACTORY)
-            // Specify the CLIENT_ID of the app that accesses the backend:
-            .setAudience(Collections.singletonList(CLIENT_ID))
-            // Or, if multiple clients access the backend:
-            //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
-            .build();
 
     private final HttpServletRequest request;
+
+    private final GoogleIdTokenVerifier verifier;
+
+    @Autowired
+    UserService(HttpServletRequest request,
+                @Value("google.auth.client_id") String clientId) {
+        this.request = request;
+        this.verifier = new GoogleIdTokenVerifier.Builder(TRANSPORT, JSON_FACTORY)
+                // Specify the CLIENT_ID of the app that accesses the backend:
+                .setAudience(Collections.singletonList(clientId))
+                // Or, if multiple clients access the backend:
+                //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+                .build();
+    }
 
     public String getUserEmail() {
         return getUserEmail(getTokenFromRequest(request));
@@ -39,7 +46,7 @@ public class UserService {
     @SneakyThrows
     private GoogleIdToken getTokenFromRequest(final HttpServletRequest request) {
         String token = request.getHeader("X-Auth-Token");
-        return VERIFIER.verify(token);
+        return verifier.verify(token);
     }
 
     private String getUserEmail(GoogleIdToken token) {
