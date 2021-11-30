3a4fe99972eb63c91a02176d662fdc96c86543e2
ddeliopoulos <ddeliopoulos@gmail.com>
2021-11-30 12:43:30 -0500

    journal entries dont get deleted, added transactional

--- src/main/java/ddeliopoulos/github/gardenjournal/journalentry/PlantRemovedEventListener.java
+++ src/main/java/ddeliopoulos/github/gardenjournal/journalentry/PlantRemovedEventListener.java
@@ -4,6 +4,7 @@ import ddeliopoulos.github.gardenjournal.plant.api.PlantRemovedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Component
@@ -12,6 +13,7 @@ class PlantRemovedEventListener implements ApplicationListener<PlantRemovedEvent
     private final JournalService journalService;
 
     @Override
+    @Transactional
     public void onApplicationEvent(PlantRemovedEvent event) {
         journalService.removeAllJournalEntries(event.getPlantId());
     }
