03510d94d9f06038308c31df1991df75edbc5b42
ddeliopoulos <ddeliopoulos@gmail.com>
2021-12-19 12:07:51 -0500

    added more selenium tests

--- src/ui-test/java/ddeliopoulos/github/gardenjournal/HomePageTest.java
+++ src/ui-test/java/ddeliopoulos/github/gardenjournal/HomePageTest.java
@@ -1,8 +1,9 @@
 package ddeliopoulos.github.gardenjournal;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -12,10 +13,7 @@ import java.time.Duration;
 
 import static ddeliopoulos.github.gardenjournal.WebDriverManager.getCurrentUserEmail;
 import static ddeliopoulos.github.gardenjournal.WebDriverManager.loginWithTestToken;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.openqa.selenium.support.ui.ExpectedConditions.urlContains;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class HomePageTest {
@@ -25,19 +23,25 @@ class HomePageTest {
     private static By ADD_PLANT_BUTTON = By.xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/div/div/div[2]/div/button");
     private static By WATERING_BUTTON = By.cssSelector("#water-btn > div > div");
     private static By SUBMIT_PLANT_BUTTON = By.xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/div/div/div[2]/div/div[2]/div[2]/button");
-
-
+    private static By CLICKABLE_PLANT_CARD = By.xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/div/div/div[3]/div/div");
+    private static By ADD_JOURNAL_BUTTON = By.xpath("/html/body/div[1]/div[1]/section/div/div/div/div[2]/p/button");
+    private static By SUBMIT_JOURNAL_BUTTON = By.cssSelector("#modals > div > div > div > button.add-journal-entry-close");
 
     @Test
     void whenNavigatingToHomePage_shouldRedirectToLoginPage() {
         // create a driver
         final WebDriver driver = WebDriverManager.getDriver();
 
+//        driver.manage().deleteAllCookies();
+//        driver.get("chrome://settings/clearBrowserData");
+//        driver.findElement(By.xpath("//settings-ui")).sendKeys(Keys.ENTER);
+
         // go to main page
         driver.navigate().to(BASE_URL);
 
         // wait until we are redirected
-        new WebDriverWait(driver, Duration.ofSeconds(3)).until(urlContains("/login"));
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.urlContains("/login"));
+
 
         // verify we are on the login page
         assertEquals(BASE_URL + "/login", driver.getCurrentUrl());
@@ -57,6 +61,32 @@ class HomePageTest {
     }
 
     @Test
+    void whenClickingAddPlantButton_thenSubmitButtonAndPlantImageIsVisible() {
+        // create a driver
+        final WebDriver driver = WebDriverManager.getDriver();
+
+        // go to main page
+        driver.navigate().to(BASE_URL);
+        loginWithTestToken();
+        driver.navigate().to(BASE_URL);
+
+        // wait to navigate to home
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        // clicks button to add plant
+        driver.findElement(ADD_PLANT_BUTTON).click();
+
+        // waits for form to pop up
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        // checks if submit button is displayed
+        assertTrue(driver.findElement(SUBMIT_PLANT_BUTTON).isDisplayed());
+
+        //check is plant image is visible
+        assertTrue(driver.findElement(By.cssSelector("#add-plant-btn > div.modal > div.container > div > img")).isDisplayed());
+    }
+
+    @Test
     void whenAddingAPlant_wateringButtonShouldBeAvailable() {
         // create a driver
         final WebDriver driver = WebDriverManager.getDriver();
@@ -66,39 +96,153 @@ class HomePageTest {
         loginWithTestToken();
         driver.navigate().to(BASE_URL);
 
-        // add plant button is available
+        // wait to navigate to home
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        // checks if plant button is visible
         assertTrue(driver.findElement(ADD_PLANT_BUTTON).isDisplayed());
 
-        // when clicking on add plant button, form pops up
-        driver.findElement(ADD_PLANT_BUTTON).click();
 //        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.visibilityOf(
 //                driver.findElement(By.cssSelector("#add-plant-btn > div.modal > div.container > label:nth-child(2)"))
 //        ));
+
+        // clicks button to add plant
+        driver.findElement(ADD_PLANT_BUTTON).click();
+
+        // checks if add plant form popped up
         assertEquals("Plant name:", driver.findElement(By.cssSelector("#add-plant-btn > div.modal > div.container > label:nth-child(2)")).getText());
 
+        // add inputs for a plant
+        addPlantInput();
+
+        // checks if watering button is visible
+        assertTrue(driver.findElement(WATERING_BUTTON).isDisplayed());
+    }
+
+    @Test
+    void whenAddingAPlant_allInputsMatchTheCreatedPlantData() {
+        final WebDriver driver = WebDriverManager.getDriver();
+
+        // go to main page
+        driver.navigate().to(BASE_URL);
+        loginWithTestToken();
+        driver.navigate().to(BASE_URL);
+
+        // wait to navigate to home
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        driver.findElement(ADD_PLANT_BUTTON).click();
+
+        // wait to navigate to home
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        addPlantInput();
+
+        // wait to navigate to home
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        assertTrue(driver.findElement(CLICKABLE_PLANT_CARD).isDisplayed());
+
+        assertEquals("Christini Plant", driver.findElement(By.cssSelector("#home > div > div > div > div > div > div.single-plant-container > div > div > a > h2")).getText());
+//        assertEquals("CutiePatootie  08/31/1990", driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/div/div/div[3]/div/div/a/div[2]/p")).getText());
+//        assertEquals("08/31/1990", driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/div/div/div[3]/div/div/a/div[2]/p/b/text()[2]")).getText());
+
+    }
+
+    @Test
+    void whenAddingAJournalEntryAndRemovingIt_shouldNotBePresent() {
+        final WebDriver driver = WebDriverManager.getDriver();
+
+        // go to main page
+        driver.navigate().to(BASE_URL);
+        loginWithTestToken();
+        driver.navigate().to(BASE_URL);
+
+        driver.findElement(ADD_PLANT_BUTTON).click();
+
+        // wait to navigate to home
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        // add inputs for a plant
+        addPlantInput();
+
+        // wait for plant to be submitted
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        // check to see plant card was created and visible
+        assertTrue(driver.findElement(CLICKABLE_PLANT_CARD).isDisplayed());
+
+        //click on created plant
+        driver.findElement(CLICKABLE_PLANT_CARD).click();
+
+        // wait to navigate to plant details page
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        // checks that we are on the correct page via url string
+        assertTrue(driver.getCurrentUrl().contains("/plant-details"));
+
+        // checks if the add journal button is available
+        assertTrue(driver.findElement(ADD_JOURNAL_BUTTON).isDisplayed());
+
+        // clicks on add journal button
+        driver.findElement(ADD_JOURNAL_BUTTON).click();
+
+        // wait for journal options to pop up
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        // checks to see that the add text journal entry button is available
+        assertTrue(driver.findElement(By.cssSelector("#modals > div > div > div > button.journal-icon-txt > img")).isDisplayed());
+
+        // click on add text journal entry
+        driver.findElement(By.cssSelector("#modals > div > div > div > button.journal-icon-txt > img")).click();
+
+        // adds input for a text journal entry
+        WebElement textJournalEntry = driver.findElement(By.cssSelector("#styled"));
+        textJournalEntry.sendKeys("I IS MAGICAL ROBOT HERE TO WRITE A JOURNAL ENTRY");
+
+        // checks that submit journal button is available
+        assertTrue(driver.findElement(SUBMIT_JOURNAL_BUTTON).isDisplayed());
+
+        // clicks on submit journal
+        driver.findElement(SUBMIT_JOURNAL_BUTTON).click();
+
+        // waits for the creating of new journal entry
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        // checks if the journal entry has been added and displayed
+        assertTrue(driver.findElement(By.xpath("/html/body/div[1]/div[1]/section/div/div/div/div[3]/div/div/div")).isDisplayed());
+        assertEquals("I IS MAGICAL ROBOT HERE TO WRITE A JOURNAL ENTRY", driver.findElement(By.xpath("/html/body/div[1]/div[1]/section/div/div/div/div[3]/div/div/div/div[2]/div/div[2]")).getText());
+
+        // checks to see if delete icon for removing plant is available and clicks it for removal
+        assertTrue(driver.findElement(By.xpath("/html/body/div[1]/div[1]/section/div/div/div/div[3]/div/div/div/div[2]/div/div[1]/i")).isDisplayed());
+        driver.findElement(By.xpath("/html/body/div[1]/div[1]/section/div/div/div/div[3]/div/div/div/div[2]/div/div[1]/i")).click();
+
+        // waits for the plant created to be removed
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        // checks weather the journal entry has been removed
+        boolean textJournalIsPresent = driver.findElements(By.xpath("/html/body/div[1]/div[1]/section/div/div/div/div[3]/div/div/div")).size() > 0;
+        assertFalse(textJournalIsPresent);
+
+    }
+
+    // adds plant input
+    private void addPlantInput() {
+        final WebDriver driver = WebDriverManager.getDriver();
+
+        // finds element inputs for adding plant
         WebElement plantNameInput = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/div/div/div[2]/div/div[2]/div[2]/input[1]"));
         WebElement plantTypeInput = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/div/div/div[2]/div/div[2]/div[2]/input[2]"));
         WebElement plantDatePlantedInput = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/div/div/div[2]/div/div[2]/div[2]/input[3]"));
         WebElement plantWateringSchedule = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/div/div/div[2]/div/div[2]/div[2]/input[4]"));
 
+        // add inputs for plant
         plantNameInput.sendKeys("Christini Plant");
         plantTypeInput.sendKeys("CutiePatootie");
         plantDatePlantedInput.sendKeys("08/31/1990");
         plantWateringSchedule.sendKeys("2");
 
-        assertTrue(driver.findElement(SUBMIT_PLANT_BUTTON).isDisplayed());
-
+        // clicks submit to add the plant
         driver.findElement(SUBMIT_PLANT_BUTTON).click();
-
-        assertTrue(driver.findElement(WATERING_BUTTON).isDisplayed());
-
-        //assertThat(driver.findElement(WATERING_BUTTON).isDisplayed()).isTrue();
-        // if we click button
-        // does it change color?
-        // is the journal entry created?
-    }
-
-    private void plantExists() {
-        // TODO: create a plant here
     }
 }
